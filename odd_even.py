30f9628e55556dbd6560a3ea27bbe072b9dfb376
Yassir future <yassirmarrah@gmail.com>
2022-03-23 23:37:43 -0400

    code for even or odd

---
+++ odd_even.py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+while 1:
+    num=int(input("What is your number?\n"))
+
+    if (num % 2) == 0:
+        print(num,"is Even")
+    else:
+        print(num, "is odd")
