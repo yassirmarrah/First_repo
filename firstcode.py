895033479bd4b7466521a2781b3eb49eb6329e2c
Yassir future <yassirmarrah@gmail.com>
2022-03-23 23:14:59 -0400

    first commit

---
+++ firstcode.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+name = input("what's your name?\n")
+age=int(input("What is your age?\n"))
+
+print("Hi "+name + " you are born in " , (2022-age))
+
